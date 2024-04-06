2256a48907b1c99278d4e0a3dd2f8d7e325b1909
herma <49307226+hermaplusplus@users.noreply.github.com>
2024-04-07 00:48:35 +0100

    fixe 6

--- main.py
+++ main.py
@@ -351,7 +351,7 @@ async def playerdata(interaction:discord.Interaction):
             await interaction.followup.send(f"Only {HIGH_STAFF_REFER} and {OTHER_APPROVER_REFER} can generate Ckey lists.", ephemeral=True)
             return
         os.system("csvtool format '%(2)\\n' accountlinks.csv > playerdata.txt")
-        await interaction.response.send_message(content=f"Generated <t:{int((datetime.now()).timestamp())}:d>.", file=discord.File(open("playerdata.txt", 'rb'), filename="playerdata.txt"), ephemeral=True)
+        await interaction.response.send_message(content=f"Generated <t:{int((datetime.now()).timestamp())}:f>.", file=discord.File(open("playerdata.txt", 'rb'), filename="playerdata.txt"), ephemeral=True)
 
 @client.event
 async def on_message(message):
