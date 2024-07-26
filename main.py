fab6c38744f5998501a6244a7f60f65d1fd03a6b
herma <49307226+hermaplusplus@users.noreply.github.com>
2024-07-26 02:11:38 +0100

    More fix

--- main.py
+++ main.py
@@ -462,12 +462,12 @@ async def toggledevping(interaction:discord.Interaction):
     app_commands.Choice(name='event', value=2)
 ])
 async def toggleping(interaction:discord.Interaction, role: int):
-    role = [UPTIME_PING_ROLE_ID, DEV_PING_ROLE_ID, EVENT_PING_ROLE_ID][role]
-    if role not in [r.id for r in interaction.user.roles]:
-        await interaction.user.add_roles(discord.Object(role.value))
+    role_id = [UPTIME_PING_ROLE_ID, DEV_PING_ROLE_ID, EVENT_PING_ROLE_ID][role]
+    if role_id not in [r.id for r in interaction.user.roles]:
+        await interaction.user.add_roles(discord.Object(role_id))
         await interaction.response.send_message(f"You will be pinged for {role.name} announcements! 🎺", ephemeral=True)
     else:
-        await interaction.user.remove_roles(discord.Object(role.value))
+        await interaction.user.remove_roles(discord.Object(role_id))
         await interaction.response.send_message(f"You will no longer be pinged for {role.name} announcements. 💤", ephemeral=True)
 
 @client.event
